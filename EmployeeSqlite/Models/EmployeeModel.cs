7794ea5efaacf4d05765ad269a8846b11a0f97cd
Boukari Yameogo <yameogo.boukari@gmail.com>
2019-09-27 00:55:01 -0400

    Add project files.

---
+++ EmployeeSqlite/Models/EmployeeModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EmployeeSqlite.Models
+{
+    public class EmployeeModel: BaseModel
+    {
+        private string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                _FirstName = value;
+                RaisePropertyChanged("FirstName");
+            }
+        }
+
+        private string _Lastname;
+
+        public string LastName
+        {
+            get { return _Lastname; }
+            set { _Lastname = value; RaisePropertyChanged("LastName"); }
+        }
+
+        private string _Position;
+
+        public string Position
+        {
+            get { return _Position; }
+            set { _Position = value; RaisePropertyChanged("Position"); }
+        }
+
+        private string _Manager;
+
+        public string Manager
+        {
+            get { return _Manager; }
+            set { _Manager = value; RaisePropertyChanged("Manager"); }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                RaisePropertyChanged("Age");
+            }
+        }
+
+        public EmployeeModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                FirstName = "Yameogo";
+                LastName = "Abou";
+                Manager = "Yameogo";
+                Position = "CEO";
+            }
+        }
+    }
+}
---
+++ EmployeeSqlite/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeSqlite.Models;
+
+namespace EmployeeSqlite.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public EmployeeModel EmpModel { get; set; }
+        public EmployeeViewModel()
+        {
+            EmpModel = new EmployeeModel();
+        }
+
+    }
+}
